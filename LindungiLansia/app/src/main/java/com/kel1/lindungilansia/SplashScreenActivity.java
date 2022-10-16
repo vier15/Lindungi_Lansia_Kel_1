f467e420a6b4e41d1661cd6cf8dc6cc4eea2c794
Raihan "VieR" Fitrah <raihanfitrah15@gmail.com>
2022-10-16 21:06:22 +0700

    Initiation Commit
    Commit pertama kali

---
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.kel1.lindungilansia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
