30925865dd999208a18ddd2c6b4fe80b52bb9048
Raihan "VieR" Fitrah <raihanfitrah15@gmail.com>
2022-10-22 22:31:13 +0700

    Update 4.1
    Revisi Splash Screen, Login, Register, Role Register

--- LindungiLansia/app/src/main/java/com/kel1/lindungilansia/MainActivity.java
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/MainActivity.java
@@ -3,13 +3,15 @@ package com.kel1.lindungilansia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_jadwal_obat);
+        setContentView(R.layout.fragment_caregiver_help);
         getSupportActionBar().hide();
     }
 }
