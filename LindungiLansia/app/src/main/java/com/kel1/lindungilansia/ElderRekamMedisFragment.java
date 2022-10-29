c491fb12864ced5cad1d668e4202f653ed1f0fe4
Raihan "VieR" Fitrah <raihanfitrah15@gmail.com>
2022-10-29 14:40:32 +0700

    Update 7.0
    - Create elder rekam medis (tanpa ui)
    - Navigasi fragment login -> register dan fragment login -> home

--- LindungiLansia/app/src/main/java/com/kel1/lindungilansia/ElderRekamMedisFragment.java
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/ElderRekamMedisFragment.java
@@ -3,19 +3,17 @@ package com.kel1.lindungilansia;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link ElderRekamMedisFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class ElderRekamMedisFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public ElderRekamMedisFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment ElderRekamMedisFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static ElderRekamMedisFragment newInstance(String param1, String param2) {
+        ElderRekamMedisFragment fragment = new ElderRekamMedisFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,24 +58,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
-
-        Button btnDaftarLogin = view.findViewById(R.id.btnDaftarLogin);
-        btnDaftarLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment);
-            }
-        });
-
-        Button btnMasukLogin = view.findViewById(R.id.btnMasukLogin);
-        btnMasukLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_elderHomeFragment);
-            }
-        });
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_elder_rekam_medis, container, false);
     }
 }
