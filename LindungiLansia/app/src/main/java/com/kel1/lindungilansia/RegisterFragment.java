9a89baf0b5d7adb48507a561520a8c34c16c89af
Raihan "VieR" Fitrah <raihanfitrah15@gmail.com>
2022-10-30 07:23:29 +0700

    Update 8.0
    - Navigasi dari register kembali ke login menggunakan tombol batal dan dari register menuju role register
    - Palet warna pada dark mode diganti

--- LindungiLansia/app/src/main/java/com/kel1/lindungilansia/RegisterFragment.java
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/RegisterFragment.java
@@ -3,10 +3,12 @@ package com.kel1.lindungilansia;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,24 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        Button btnDaftarRegister = view.findViewById(R.id.btnDaftarRegister);
+        btnDaftarRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_roleRegisterFragment);
+            }
+        });
+
+        Button btnBatalRegister = view.findViewById(R.id.btnBatalRegister);
+        btnBatalRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_loginFragment);
+            }
+        });
+
+        return view;
     }
 }
