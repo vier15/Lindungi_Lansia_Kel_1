6de2c3799626949e0df1e6117498010c6b851ac1
Nyoman Ari S <90953569+nyomanari21@users.noreply.github.com>
2022-10-31 15:20:57 +0700

    Update 9.1
    Update:
    - Tambah Fragment Caregiver Lihat Profil Elder
    - Tambah navigasi dari caregiver home ke caregiver lihat profil elder
    - Tambah navigasi dari caregiver lihat profil elder ke caregiver home

--- LindungiLansia/app/src/main/java/com/kel1/lindungilansia/CaregiverLihatProfilElderFragment.java
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/CaregiverLihatProfilElderFragment.java
@@ -2,20 +2,20 @@ package com.kel1.lindungilansia;
 
 import android.os.Bundle;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CaregiverHomeFragment#newInstance} factory method to
+ * Use the {@link CaregiverLihatProfilElderFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CaregiverHomeFragment extends Fragment {
+public class CaregiverLihatProfilElderFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class CaregiverHomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CaregiverHomeFragment() {
+    public CaregiverLihatProfilElderFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class CaregiverHomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CaregiverHomeFragment.
+     * @return A new instance of fragment CaregiverLihatProfilElderFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CaregiverHomeFragment newInstance(String param1, String param2) {
-        CaregiverHomeFragment fragment = new CaregiverHomeFragment();
+    public static CaregiverLihatProfilElderFragment newInstance(String param1, String param2) {
+        CaregiverLihatProfilElderFragment fragment = new CaregiverLihatProfilElderFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,14 +60,14 @@ public class CaregiverHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_caregiver_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_caregiver_lihat_profil_elder, container, false);
 
-        // Navigasi ke lihat profil elder
-        ConstraintLayout clElderCaregiverHome1 = view.findViewById(R.id.clElderCaregiverHome1);
-        clElderCaregiverHome1.setOnClickListener(new View.OnClickListener() {
+        // Navigasi ke halaman caregiver home
+        Button btnKembaliCaregiverLihatProfilElder = view.findViewById(R.id.btnKembaliCaregiverLihatProfilElder);
+        btnKembaliCaregiverLihatProfilElder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_caregiverHomeFragment_to_caregiverLihatProfilElderFragment);
+                Navigation.findNavController(view).navigate(R.id.action_caregiverLihatProfilElderFragment_to_caregiverHomeFragment);
             }
         });
 
