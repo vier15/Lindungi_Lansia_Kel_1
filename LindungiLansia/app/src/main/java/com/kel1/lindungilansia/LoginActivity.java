ab1f221f82aaf3866c33e0807e6e3df6307ca9fd
Muhamad Firmansyah <firmann1952@gmail.com>
2022-11-27 21:10:15 +0700

    Update 16.3
    MENCOBA FIREBASE

---
+++ LindungiLansia/app/src/main/java/com/kel1/lindungilansia/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.kel1.lindungilansia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DbUser dbUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        dbUser = new DbUser(getApplicationContext());
+        dbUser.open();
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+
+        NavigationView navigationView = findViewById(R.id.nvElderHomeSidebar);
+        navigationView.setItemIconTintList(null);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.navhost_main);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    protected void onDestroy() {
+        dbUser.close();
+        super.onDestroy();
+    }
+
+
+}
